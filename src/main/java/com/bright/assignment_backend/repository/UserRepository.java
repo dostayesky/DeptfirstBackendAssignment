9ff2891814ef60a2782677b7529b566ed4abbfae
dostayesky <brightkav2@gmail.com>
2025-04-05 15:15:31 +0700

    feat create User Repository

--- src/main/java/com/bright/assignment_backend/repository/UserRepository.java
+++ src/main/java/com/bright/assignment_backend/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.bright.assignment_backend.repository;
 import com.bright.assignment_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<String, User> {
+public interface UserRepository extends JpaRepository< User,String> {
 
 }
